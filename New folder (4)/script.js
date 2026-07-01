288e359a079b7613705cf70e6aa7577692eef639
sabafatima7860011-lang <sabafatima7860011@gmail.com>
2026-07-01 13:01:22 +0530

    Add files via upload

---
+++ New folder (4)/script.js
@@ -0,0 +1,9 @@
+
+const greetingElement = document.getElementById('greeting');
+greetingElement.textContent = "Hi, I'm naina!";
+
+const introElement = document.getElementById('intro');
+
+introElement.style.color = "purple";
+
+introElement.style.fontSize = "40px";
