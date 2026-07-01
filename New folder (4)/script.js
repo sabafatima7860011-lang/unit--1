8cf3602137766ddeec88a109f8ce3dba05f4a0db
sabafatima7860011-lang <sabafatima7860011@gmail.com>
2026-07-01 13:05:27 +0530

    Delete New folder (4) directory

--- New folder (4)/script.js
+++
@@ -1,9 +0,0 @@
-
-const greetingElement = document.getElementById('greeting');
-greetingElement.textContent = "Hi, I'm naina!";
-
-const introElement = document.getElementById('intro');
-
-introElement.style.color = "purple";
-
-introElement.style.fontSize = "40px";
